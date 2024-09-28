119d2d4a1df992946505a20bc0c32b2b7a50fcc8
ZeinabAbdoo <zeinab.abdelghaffar@monglish.uk>
2024-09-28 16:21:04 +0300

    Add the Full Edits in Section 5 in the AR $ EN versions

--- src/components/ar/AdultsSectionFive.vue
+++ src/components/ar/AdultsSectionFive.vue
@@ -18,7 +18,7 @@ export default {
 </script>
 
 <style scoped>
-.kids-sec4{
+.kids-sec4 {
   background-image: url('@/assets/images/bg2.png');
   background-size: cover;
   background-position: center;
@@ -29,6 +29,7 @@ export default {
   font-weight: 500;
   padding: 2em 2em 8em 2em;
 }
+
 .immerse-row {
   display: flex;
   flex-direction: column;
@@ -38,12 +39,14 @@ export default {
   margin: 1em 0;
   font-family: 'DIN Next LT Arabic', sans-serif;
 }
+
 .immerse-row h3 {
   color: #fff;
-  font-size: 2.8rem;
+  font-size: 2.5rem;
   font-family: 'DIN Next LT Arabic';
   font-weight: 500;
 }
+
 .immerse-row p {
   font-size: 1.275rem;
   color: #fff;
@@ -51,15 +54,19 @@ export default {
   max-width: 70%;
   margin: 0 auto;
 }
-.kids-sec4-imgs{
+
+.kids-sec4-imgs {
   display: flex;
   justify-content: center;
   align-items: center;
   padding-top: 4%;
+  width: 100%; /* Full width for mobile view */
 }
-.kids-sec4-imgs img{
-    width: 80%;
+
+.kids-sec4-imgs img {
+  width: 80%;
 }
+
 @media (min-width: 769px) {
   .immerse-row {
     flex-direction: row;
@@ -67,16 +74,17 @@ export default {
     text-align: right;
   }
   .immerse-row h3 {
-    width: 35%;
+    width: 40%;
     margin: 0;
-    padding-right: 50px;
+    padding-right: 30px;
   }
   .immerse-row p {
-    width: 65%;
+    width: 60%;
     margin: 0;
     padding-left: 25px;
   }
 }
+
 @media (max-width: 1200px) {
   .kids-sec4 h3 {
     font-size: 2.2rem;
@@ -89,32 +97,45 @@ export default {
     padding: 1% 15% 5% 15%;
   }
 }
+
 @media (max-width: 768px) {
-  .immerse-row{
+  .kids-sec4 {
+    background-image: url('@/assets/images/bg.jpg'); 
+    padding: 2em 0 2em 0;
+  }
+  
+  .immerse-row {
     margin: 0;
   }
+  
   .kids-sec4 h3 {
     font-size: 1.8rem;
     padding: 0;
     margin: 0;
   }
+  
   .kids-sec4 h1 {
     font-size: 2rem;
     padding: 0;
     margin: 0;
   }
+  
   .kids-sec4 p {
     font-size: 1.25rem;
-    color: darkgrey;
     width: 100%;
     padding: 0;
     margin: 0;
   }
+
   .kids-sec4-imgs {
-    padding-top: 0;
-}
-  .kids-sec4-imgs img{
+    padding: 1em 0;
+    background-color: white; 
     width: 100%;
-}
+    margin-top: 1em;
+  }
+
+  .kids-sec4-imgs img {
+    width: 100%; /* Ensure image takes full width of the container */
+  }
 }
 </style>
--- src/components/en/AdultsSectionFive.vue
+++ src/components/en/AdultsSectionFive.vue
@@ -2,9 +2,9 @@
   <!--Section Four Content-->
   <div class="kids-sec4">
     <div class="immerse-row">
-      <h3>معتمدون دوليا <br> والجودة العالية في كل خطوة!</h3>
-      <p>تُعد الأكاديمية جهة معتمدة لتقديم الدورات التدريبية ( Accredited Training Provider ) وفقاً لإعتمادها من قِبل المنظمة الدولية لجودة التدريب – الولايات المتحدة الأمريكية IOTQ USA. يشير هذا الاعتماد إلى جودة الدورات التدريبية، وهو يشمل جميع كيانات ومواقع وأنشطة أكاديمة مونجلش العالمية.</p>
-    </div>
+  <h3>Internationally Accredited <br> and High Quality at Every Step!</h3>
+  <p>The academy is an accredited entity for providing training courses (Accredited Training Provider) in accordance with its accreditation by the International Organization for Training Quality – USA IOTQ USA. This accreditation indicates the quality of the training courses, and it encompasses all entities, locations, and activities of the Monglish Global Academy.</p>
+</div>
     <div class="kids-sec4-imgs">
         <img src="@/assets/images/Logos.png"/>
     </div>    
@@ -18,17 +18,18 @@ export default {
 </script>
 
 <style scoped>
-.kids-sec4{
+.kids-sec4 {
   background-image: url('@/assets/images/bg2.png');
   background-size: cover;
   background-position: center;
-  direction: rtl;
+  direction: ltr;
   text-align: center;
   color: #fff;
   font-family: 'DIN Next LT Arabic';
   font-weight: 500;
   padding: 2em 2em 8em 2em;
 }
+
 .immerse-row {
   display: flex;
   flex-direction: column;
@@ -38,12 +39,14 @@ export default {
   margin: 1em 0;
   font-family: 'DIN Next LT Arabic', sans-serif;
 }
+
 .immerse-row h3 {
   color: #fff;
-  font-size: 2.8rem;
+  font-size: 2.5rem;
   font-family: 'DIN Next LT Arabic';
   font-weight: 500;
 }
+
 .immerse-row p {
   font-size: 1.275rem;
   color: #fff;
@@ -51,32 +54,37 @@ export default {
   max-width: 70%;
   margin: 0 auto;
 }
-.kids-sec4-imgs{
+
+.kids-sec4-imgs {
   display: flex;
   justify-content: center;
   align-items: center;
   padding-top: 4%;
+  width: 100%; /* Full width for mobile view */
 }
-.kids-sec4-imgs img{
-    width: 80%;
+
+.kids-sec4-imgs img {
+  width: 80%;
 }
+
 @media (min-width: 769px) {
   .immerse-row {
     flex-direction: row;
     justify-content: center;
-    text-align: right;
+    text-align: left;
   }
   .immerse-row h3 {
-    width: 35%;
+    width: 40%;
     margin: 0;
-    padding-right: 50px;
+    padding-left: 30px;
   }
   .immerse-row p {
-    width: 65%;
+    width: 60%;
     margin: 0;
-    padding-left: 25px;
+    padding-right: 25px;
   }
 }
+
 @media (max-width: 1200px) {
   .kids-sec4 h3 {
     font-size: 2.2rem;
@@ -89,32 +97,45 @@ export default {
     padding: 1% 15% 5% 15%;
   }
 }
+
 @media (max-width: 768px) {
-  .immerse-row{
+  .kids-sec4 {
+    background-image: url('@/assets/images/bg.jpg'); 
+    padding: 2em 0 2em 0;
+  }
+  
+  .immerse-row {
     margin: 0;
   }
+  
   .kids-sec4 h3 {
     font-size: 1.8rem;
     padding: 0;
     margin: 0;
   }
+  
   .kids-sec4 h1 {
     font-size: 2rem;
     padding: 0;
     margin: 0;
   }
+  
   .kids-sec4 p {
     font-size: 1.25rem;
-    color: darkgrey;
     width: 100%;
     padding: 0;
     margin: 0;
   }
+
   .kids-sec4-imgs {
-    padding-top: 0;
-}
-  .kids-sec4-imgs img{
+    padding: 1em 0;
+    background-color: white; 
     width: 100%;
-}
+    margin-top: 1em;
+  }
+
+  .kids-sec4-imgs img {
+    width: 100%; /* Ensure image takes full width of the container */
+  }
 }
 </style>
